d0ddab2653e7a6a663d0612e55b8dd01027acaa8
bountyhunt3r <bountyhunt3r404@gmail.com>
2023-08-16 18:35:46 +0530

    Added JSON function to write over serial for test

--- code/nano_slave_code.cpp
+++ code/nano_slave_code.cpp
@@ -5,7 +5,10 @@
 */
 
 #include <Arduino.h>
+
 #include <ArduinoJson.h>
+#include <Joystick.h>
+#include <AxisJoystick.h>
 //#include <RF24.h>
 #include <SPI.h>
 
@@ -30,111 +33,10 @@
 #define L_Trig 5
 #define R_Trig 6
 
-/* Variables for Storing Values of Various Sensors */
-//Left JoyStick
-int L_X_VAL = 0;
-int L_Y_VAL = 0;
-bool L_PIN_STATE = false;
+//Axis JoyStick
+AxisJoystick LJOY(L_PIN, L_X, L_Y);  //Left Joystick
+AxisJoystick RJOY(R_PIN, R_X, R_Y);  //Right Joystick
 
-//Right JoyStick
-int R_X_VAL = 0;
-int R_Y_VAL = 0;
-bool R_PIN_STATE = false;
-
-//Triggers
-bool L_Trig_STATE = false;
-bool R_Trig_STATE = false;
-
-//Pots
-int L_POT_VAL = 0;
-int R_POT_VAL = 0;
-
-//Funtion for reading sensor values
-void ReadSense() {
-    //All switches
-    L_PIN_STATE = digitalRead(L_PIN);
-    R_PIN_STATE = digitalRead(R_PIN);
-    L_Trig_STATE = digitalRead(L_Trig);
-    R_Trig_STATE = digitalRead(R_Trig);
-
-    //All Joysticks and Pots
-    L_X_VAL = map(analogRead(L_X), 0, 1023, 0, 255);
-    L_Y_VAL = map(analogRead(L_Y), 0, 1023, 0, 255);
-    R_X_VAL = map(analogRead(R_X), 0, 1023, 0, 255);
-    R_Y_VAL = map(analogRead(R_Y), 0, 1023, 0, 255);
-    L_POT_VAL = analogRead(L_POT);
-    R_POT_VAL = analogRead(R_POT);
-}
-
-//For Debugging
-void PrintFunc() {
-    if (Serial.available() > 0) {
-    while (Serial.available() > 0) {
-     Serial.print(L_X_VAL);
-     Serial.print("\t");
-     Serial.print(L_Y_VAL);
-     Serial.print("\t");
-     Serial.print(R_X_VAL);
-     Serial.print("\t");
-     Serial.print(R_Y_VAL);
-     Serial.print("\t");
-     Serial.print(L_POT_VAL);
-     Serial.print("\t");
-     Serial.print(R_POT_VAL);
-     Serial.print("\t");
-     Serial.print(L_PIN_STATE);
-     Serial.print("\t");
-     Serial.print(R_PIN_STATE);
-     Serial.print("\t");
-     Serial.print(L_Trig_STATE);
-     Serial.print("\t");
-     Serial.print(R_Trig_STATE);
-     Serial.println("");
-     delay(10);
-    }
-    }
-    
-    else
-    {
-        Serial.end();
-        Serial.begin(115200);
-    }
-}
-
-bool PrintState = false;
-bool readstate = false;
-String rawdata[256] = {};
-
-//Function For Communicaton between ESP32 and ARDUINO NANO.
-void SerialCommunication() {
-    //wait for esp32 to initialise
-    
-        if (Serial.read() == 'S') {
-            readstate = true;
-            Serial.print("testok");
-        }
-    
-
-
-    /*if (Serial.available() > 0) {
-        int ReadBytes = Serial.read();
-        if (ReadBytes == 'S') {
-            PrintState = true;
-        }
-        
-        else if (ReadBytes == 'E' && PrintState) {
-            PrintState = false;
-            if (!PrintState) {
-                Serial.end();
-                Serial.begin(115200);
-            }
-        }
-    }
-*/
-    if (PrintState) {
-                Serial.println(L_X_VAL);
-            }
-}
 
 void Json_Serial_Communication() {
     // Values we want to transmit
@@ -160,7 +62,48 @@ void Json_Serial_Communication() {
   delay(1000);
 }
 
+String moveTitle(const Joystick::Move move) {
+        switch (move) {
+            case Joystick::Move::NOT:
+            return "NOT";
+            case Joystick::Move::PRESS:
+            return "PRESS";
+            case Joystick::Move::UP:
+            return "UP";
+            case Joystick::Move::DOWN:
+            return "DOWN";
+            case Joystick::Move::RIGHT:
+            return "RIGHT";
+            case Joystick::Move::LEFT:
+            return "LEFT";
+            default:
+            return "???";
+        }
+    }
+
+
+/**
+ * @brief Transmit the readings from all the sensors to ESP32 over serial
+ *        using JSON format
+ * 
+ */
+void json_over_serial_communication() {
+    //Creating JSON Document to store the sensor readings
+    StaticJsonDocument<500> sensordata;
+
+    //Default JSON doc structure
+    sensordata["L_JOY_SINGLEREAD"] = moveTitle(LJOY.singleRead());
+    sensordata["L_JOY_MULTIPLEREAD"] = moveTitle(LJOY.multipleRead());
+    sensordata["R_JOY_SINGLEREAD"] = moveTitle(RJOY.singleRead());
+    sensordata["R_JOY_MULTIPLEREAD"] = moveTitle(RJOY.multipleRead());
+
+    serializeJsonPretty(sensordata, Serial);
+}
+
+    
+
 void setup() {
+    /*
     pinMode(L_X, INPUT);
     pinMode(L_Y, INPUT);
     pinMode(L_PIN, INPUT_PULLUP);
@@ -169,16 +112,22 @@ void setup() {
     pinMode(R_PIN, INPUT_PULLUP);
     pinMode(L_POT, INPUT);
     pinMode(R_POT, INPUT);
+    */
     pinMode(L_Trig, INPUT_PULLUP);
-    pinMode(R_Trig, INPUT_PULLUP);
+    pinMode(R_Trig, INPUT_PULLUP);  
 
+    //Calibration
+    LJOY.calibrate(0, 1022, 100);
+    RJOY.calibrate(0, 1022, 100);
+    
     Serial.begin(9600);
     while (!Serial) continue;
 }
 
 void loop() {
-    ReadSense();
+    //ReadSense();
     //PrintFunc();
     //SerialCommunication();
-    Json_Serial_Communication();
+    //Json_Serial_Communication();
+    json_over_serial_communication();
 }
